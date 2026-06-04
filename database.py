36b7e925f9ca8820ecfb07017cf51dfb078d4628
sjlee5125 <sjlee5125@naver.com>
2026-06-04 09:12:24 +0900

    test5６

--- database.py
+++ database.py
@@ -5,10 +5,10 @@ import socket
 hostname = socket.gethostname()
 if hostname == "coffeechat":
     # 클라우드 서버 내부에서는 옆방 DB로 바로 접속 (localhost)
-    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:soldesk0604@localhost:5432/postgres"
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:soldesk0526@localhost:5432/postgres"
 else:
     # 팀원들 노트북에서는 클라우드 서버 DB로 원격 접속 (외부 IP)
-    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:soldesk0604@48.211.169.52:5432/postgres"
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:soldesk0526@48.211.169.52:5432/postgres"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
