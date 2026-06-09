b9b83a97b7375a034009bb452399dd33cee3255f
sjlee5125 <sjlee5125@naver.com>
2026-06-09 10:26:54 +0900

    test90

--- routers/stt.py
+++ routers/stt.py
@@ -26,7 +26,8 @@ from sqlalchemy.orm import Session
 load_dotenv()
 logger = logging.getLogger(__name__)
 router = APIRouter(tags=["STT"])
-
+AZURE_SPEECH_KEY = os.getenv("AZURE_SPEECH_KEY")
+AZURE_SPEECH_REGION = os.getenv("AZURE_SPEECH_REGION")
 # Azure SDK는 선택적 import (키가 없는 환경에서도 서버가 뜨도록)
 try:
     import azure.cognitiveservices.speech as speechsdk
